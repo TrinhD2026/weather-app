2a10983f464ac0cd3677dc83cbab2e7ba336b1e3
Trinh Doan <trinhd997@gmail.com>
2026-02-04 09:00:24 +0200

    retrieved current temperature datas

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import './App.css'
+import {useState} from 'react';
+import './App.css';
 const datas=[
     {
         date: "Monday",
@@ -905,20 +905,136 @@ const datas=[
         ]
     },
 ];
-function App() {
 
+function App() {
+    const [tempUnit,setTempUnit]=useState("\u00BAC");
+    const [speedUnit,setSpeedUnit]=useState("km/h");
+    const [precipitationUnit,setPrecipitationUnit]=useState("mm");
     const [query,setQuery]=useState("");
+    const [searchName,setSearchName]=useState("");
+    const [country,setCountry]=useState("");
+    const [day,setDay]=useState("");
+    const [date,setDate]=useState("");
+    const [month,setMonth]=useState("");
+    const [year,setYear]=useState("");
     const [hourlyTemps,setHourlyTemps]=useState([]);
     const [isSubmenuHidden,setIsSubmenuHidden]=useState(true);
-    const [feelLike,setFeelLike]=useState("");
-    const [humidity,setHumidity]=useState("");
-    const [wind,setWind]=useState("");
-    const [precipitation,setPrecipitation]=useState("");
+    const [currentTemp,setCurrentTemp]=useState(null);
+    const [feelLike,setFeelLike]=useState(null);
+    const [humidity,setHumidity]=useState(null);
+    const [windSpeed,setWindSpeed]=useState(null);
+    const [precipitation,setPrecipitation]=useState(null);
 
     const handleSelectedDayChange=(event) => {
         event.preventDefault();
         setHourlyTemps(datas.find(d => d.date.toLowerCase()===event.target.value).hourlyTemps);
     };
+
+    function convertMonth(monthNum) {
+        let month="";
+
+        switch(monthNum) {
+            case 0:
+                month="Jan";
+                break;
+            case 1:
+                month="Feb";
+                break;
+            case 2:
+                month="Mar";
+                break;
+            case 3:
+                month="Apr";
+                break;
+            case 4:
+                month="May";
+                break;
+            case 5:
+                month="Jun";
+                break;
+            case 6:
+                month="Jul";
+                break;
+            case 7:
+                month="Aug";
+                break;
+            case 8:
+                month="Sep";
+                break;
+            case 9:
+                month="Oct";
+                break;
+            case 10:
+                month="Nov";
+                break;
+            case 11:
+                month="Dec";
+                break;
+        }
+
+        return month;
+    }
+
+    function convertDay(dayNum) {
+        let day="";
+
+        switch(dayNum) {
+            case 0:
+                day="Sun";
+                break;
+            case 1:
+                day="Mon";
+                break;
+            case 2:
+                day="Tue";
+                break;
+            case 3:
+                day="Wed";
+                break;
+            case 4:
+                day="Thu";
+                break;
+            case 5:
+                day="Fri";
+                break;
+            case 6:
+                day="Sat";
+                break;
+        }
+
+        return day;
+    }
+    async function searchWeather() {
+
+        try {
+            const longlatRes=await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${query}&count=1&language=en&format=json`);
+            const longlatJson=await longlatRes.json();
+            const longtitude=longlatJson.results[0]["longitude"];
+            const latitude=longlatJson.results[0]["latitude"];
+            setSearchName(longlatJson.results[0]["name"]);
+            setCountry(longlatJson.results[0]["country"]);
+
+            const weatherData=await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longtitude}&current=apparent_temperature,precipitation,wind_speed_10m,wind_direction_10m,temperature,relative_humidity_2m`);
+            const weatherJson=await weatherData.json();
+            console.log(weatherJson["current"]);
+            setCurrentTemp(weatherJson["current"]["temperature"]);
+            setFeelLike(weatherJson["current"]["apparent_temperature"]);
+            setWindSpeed(weatherJson["current"]["wind_speed_10m"]);
+            setPrecipitation(weatherJson["current"]["precipitation"]);
+            setHumidity(weatherJson["current"]["relative_humidity_2m"]);
+            const dateTime=new Date(weatherJson["current"]["time"]);
+            setDay(convertDay(dateTime.getDay()));
+            setDate(dateTime.getDate());
+            setMonth(convertMonth(dateTime.getMonth()));
+            setYear(dateTime.getFullYear());
+            return true;
+        }
+
+        catch(error) {
+            console.log(error);
+            return false;
+        }
+    }
 
     return (
         <>
@@ -948,38 +1064,38 @@ function App() {
                         onChange={(e) => setQuery(e.target.value)}
                     />
                 </div>
-                <button className="wide-btn">Search</button>
+                <button className="wide-btn" onClick={searchWeather}>Search</button>
                 <div className="container__overall-result">
                     <picture>
-                        <source srcset="/bg-today-small.svg" media="(max-width: 52rem)" />
-                        <source srcset="/bg-today-large.svg" media="(min-width: 52rem)" />
+                        <source srcSet="/bg-today-small.svg" media="(max-width: 52rem)" />
+                        <source srcSet="/bg-today-large.svg" media="(min-width: 52rem)" />
                         <img src="/bg-today-small.svg" alt="today background image"/>
                     </picture>
                     <div className="today-result">
-                        <h2>City, Country</h2>
-                        <p>Day, Month, Year</p>
+                        <h2>{`${searchName}, ${country}`}</h2>
+                        <p>{`${day}, ${month} ${date}, ${year}`}</p>
                         <div>
                             <img src="/icon-sunny.webp"/>
-                            <p>20</p>
+                            <p>{currentTemp!=null? `${currentTemp} ${tempUnit}` : ''}</p>
                         </div>
                     </div>
                 </div>
                 <div className="container__result-details">
                     <div >
                         <label>Feels Like</label>
-                        <p>{feelLike}</p>
+                        <p>{feelLike!=null? `${feelLike} ${tempUnit}`:''}</p>
                     </div>
                     <div>
                         <label>Humidity</label>
-                        <p>{humidity}</p>
+                        <p>{humidity!=null ? `${humidity} %` : ''}</p>
                     </div>
                     <div>
                         <label>Wind</label>
-                        <p>{wind}</p>
+                        <p>{windSpeed!=null? `${windSpeed} ${speedUnit}`:''}</p>
                     </div>
                     <div>
                         <label>Precipitation</label>
-                        <p>{precipitation}</p>
+                        <p>{precipitation!=null? `${precipitation} ${precipitationUnit}` : ''}</p>
                     </div>
                 </div>
             </div>
@@ -993,8 +1109,8 @@ function App() {
                                     <p>{data.shortDate}</p>
                                     <img src={data.icon} alt="weather icon" />
                                     <div>
-                                        <p>{data.maxTemp}</p>
-                                        <p>{data.minTemp}</p>
+                                        <p>{`${data.maxTemp}\u00BA`}</p>
+                                        <p>{`${data.minTemp}\u00BA`}</p>
                                     </div>
                                 </li>
                             );
@@ -1028,7 +1144,7 @@ function App() {
                                 <li key={hourlyTemp.hour}>
                                     <img src={hourlyTemp.icon} alt="weather icon" />
                                     <p>{hourlyTemp.hour}</p>
-                                    <p>{hourlyTemp.temp}</p>
+                                    <p>{`${hourlyTemp.temp}\u00BA`}</p>
                                 </li>
                             );
                         })
